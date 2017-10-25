02f84a5cd05083c66d37ba0a669e29217939a461
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:39:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
 // assign an object to playlist and initialize the object with an 'artist name : song title' pair
 var playlist = { artistName: 'songTitle'};
 
-function updatePlaylist(playlist, artistName, songTitle)
+function updatePlaylist(playlist, artistName, songTitle) {
+  
+}
