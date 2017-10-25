6c2c0ac88f1ccda16c34cdfa7e8641fed183effb
Christian Ahn <kwanghoonahn@gmail.com>
2017-10-25 22:37:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,2 @@
-var playlist = new Object({artistName : 'songTitle'});
+var playlist = { artistName: 'songTitle'};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-  return playlist
-}
